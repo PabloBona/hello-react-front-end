16f9b4853028e2f8483cd50a5c56b178f62c28c1
PabloBona <pablobonasera@gmail.com>
2023-10-24 20:23:32 -0300

    Port updated

--- src/index.js
+++ src/index.js
@@ -6,14 +6,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = createRoot(document.getElementById('root')); // Utiliza createRoot en lugar de ReactDOM.createRoot
+const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
- 
         <App />
- 
     </Provider>
   </React.StrictMode>
 );
