f87efeff64e6b10f6b66204f8dba65de858031c1
PabloBona <pablobonasera@gmail.com>
2023-10-24 20:42:29 -0300

    Add styles

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchGreeting } from '../redux/actions';
+import './greeting.css';
 
 const Greeting = ({ greeting, fetchGreeting }) => {
   useEffect(() => {
     fetchGreeting();
   }, [fetchGreeting]);
 
-  return <div>{greeting}</div>;
+  return (
+    <div className='container'>
+      <h1 className='greeting'>{greeting}</h1>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
